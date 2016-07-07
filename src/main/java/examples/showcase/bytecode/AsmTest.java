6e612a723515f85251e2cd098f3bd58077046a37
andygulin <andygulin@hotmail.com>
2016-07-07 10:25:26 +0800

    ...

--- src/main/java/examples/showcase/bytecode/AsmTest.java
+++ src/main/java/examples/showcase/bytecode/AsmTest.java
@@ -1,5 +1,19 @@
 package examples.showcase.bytecode;
 
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.objectweb.asm.ClassReader;
+
 public class AsmTest {
 
+	private static final Logger logger = LogManager.getLogger(AsmTest.class);
+
+	@Test
+	public void read() throws IOException {
+		ClassReader reader = new ClassReader("examples/showcase/User");
+		logger.info(reader);
+	}
 }
