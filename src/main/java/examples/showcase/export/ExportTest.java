e693ee67140300c0ac10ead9fee83e30e23efe1f
andygulin <andygulin@hotmail.com>
2016-07-07 12:06:57 +0800

    ...

--- src/main/java/examples/showcase/export/ExportTest.java
+++ src/main/java/examples/showcase/export/ExportTest.java
@@ -3,6 +3,7 @@ package examples.showcase.export;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,15 +17,16 @@ public class ExportTest {
 
 	@Test
 	public void export() {
-		List<Employee> employees = Lists.newArrayList();
+		final int ROW_LENGTH = 10000;
+		List<Employee> employees = Lists.newArrayListWithExpectedSize(ROW_LENGTH);
 		final int NAME_LENGTH = 5;
-		final int ROW = 10000;
 		final String CHARS = "abcdefahijklmnopqrstuvwxyz";
-		for (int i = 1; i <= ROW; i++) {
+		for (int i = 1; i <= ROW_LENGTH; i++) {
 			employees.add(new Employee(i, RandomStringUtils.random(NAME_LENGTH, CHARS), true, new Date()));
 		}
-		final String filename = "/tmp/bb.xls";
+		final String filename = FileUtils.getTempDirectoryPath() + "bb.xls";
 		boolean result = new ExportExcel<Employee>(employees, filename, Employee.class).export();
+		logger.info(filename);
 		logger.info(result);
 	}
 }
