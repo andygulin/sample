4ed2f8d64abfb1eb414f75a26fe9989c916b3b86
andygulin <andygulin@hotmail.com>
2016-07-12 11:38:39 +0800

    ...

--- src/main/java/examples/showcase/http/OKHttpTest.java
+++ src/main/java/examples/showcase/http/OKHttpTest.java
@@ -6,9 +6,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class OKHttpTest extends AbstractHttpTest {
 
