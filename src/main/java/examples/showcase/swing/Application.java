916e9dd91acddf948911a2d26aca1d8be03d5694
andygulin <andygulin@hotmail.com>
2016-07-13 12:22:48 +0800

    ...

--- src/main/java/examples/showcase/swing/Application.java
+++ src/main/java/examples/showcase/swing/Application.java
@@ -2,11 +2,8 @@ package examples.showcase.swing;
 
 public class Application {
 	public static void main(String[] args) {
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				new LoginFrame();
-			}
+		new Thread(() -> {
+			new LoginFrame();
 		}).start();
 	}
 }
