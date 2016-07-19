e308aa6b310042c6e8a073e839b5dc35f8f142b1
andygulin <andygulin@hotmail.com>
2016-07-19 17:14:34 +0800

    ...

---
+++ src/main/java/examples/showcase/markdown/MarkdownTest.java
@@ -0,0 +1,24 @@
+package examples.showcase.markdown;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.markdown4j.Markdown4jProcessor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import examples.showcase.memcached.MemcachedTest;
+
+public class MarkdownTest {
+
+	private static final Logger logger = LogManager.getLogger(MemcachedTest.class);
+
+	@Test
+	public void process() throws IOException {
+		Resource resource = new ClassPathResource("examples/showcase/markdown/test.md");
+		String text = new Markdown4jProcessor().process(resource.getFile());
+		logger.info(text);
+	}
+}
---
+++ src/main/java/examples/showcase/markdown/test.md
@@ -0,0 +1,18 @@
+# H1
+## H2
+### H3
+#### H4
+##### H5
+###### H6
+
+> 1234
+
+* 1
+* 2
+* 3
+
+***
+
+[baidu](http://www.baidu.com)
+
+![](https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png)
