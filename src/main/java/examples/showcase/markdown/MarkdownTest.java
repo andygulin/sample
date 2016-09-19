5bbcb543e3669bf901535a6259e7aaf9f3be431a
andygulin <andygulin@hotmail.com>
2016-09-19 12:03:35 +0800

    ...

--- src/main/java/examples/showcase/markdown/MarkdownTest.java
+++ src/main/java/examples/showcase/markdown/MarkdownTest.java
@@ -1,7 +1,9 @@
 package examples.showcase.markdown;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
@@ -20,5 +22,8 @@ public class MarkdownTest {
 		Resource resource = new ClassPathResource("examples/showcase/markdown/test.md");
 		String text = new Markdown4jProcessor().process(resource.getFile());
 		logger.info(text);
+		File file = new File(FileUtils.getTempDirectoryPath(), "test.html");
+		FileUtils.writeStringToFile(file, text, "UTF-8", false);
+		logger.info(file);
 	}
 }
