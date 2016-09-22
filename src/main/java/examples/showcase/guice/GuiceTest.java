f1e01cb19e4b960a6cd0533724808a1b423bfdf5
andygulin <andygulin@hotmail.com>
2016-09-22 13:53:45 +0800

    ...

--- src/main/java/examples/showcase/guice/GuiceTest.java
+++ src/main/java/examples/showcase/guice/GuiceTest.java
@@ -2,6 +2,7 @@ package examples.showcase.guice;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.google.inject.Guice;
@@ -11,17 +12,24 @@ public class GuiceTest {
 
 	private static final Logger logger = LogManager.getLogger(GuiceTest.class);
 
+	private Injector injector;
+
+	@Before
+	public void init() {
+		injector = Guice.createInjector();
+	}
+
 	@Test
 	public void helloServiceTest() {
-		Injector injector = Guice.createInjector();
 		HelloService helloService = injector.getInstance(HelloService.class);
+		logger.info(helloService.getClass().getName());
 		logger.info(helloService.sayHello("java"));
 	}
 
 	@Test
 	public void authServiceTest() {
-		Injector injector = Guice.createInjector();
 		AuthService authService = injector.getInstance(AuthService.class);
+		logger.info(authService.getClass().getName());
 		logger.info(authService.login("root", "root"));
 	}
 }
