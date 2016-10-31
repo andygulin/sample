7211178754e2932b6b77472a1f8c93c9d0542491
andygulin <andygulin@hotmail.com>
2016-10-31 10:53:04 +0800

    ...

--- src/main/java/examples/showcase/guice/AuthService.java
+++
@@ -1,9 +0,0 @@
-package examples.showcase.guice;
-
-import com.google.inject.ImplementedBy;
-
-@ImplementedBy(FileAuthServiceImpl.class)
-public interface AuthService {
-
-	boolean login(String username, String password);
-}
--- src/main/java/examples/showcase/guice/HelloService.java
+++
@@ -1,9 +0,0 @@
-package examples.showcase.guice;
-
-import com.google.inject.ImplementedBy;
-
-@ImplementedBy(HelloServiceImpl.class)
-public interface HelloService {
-
-	String sayHello(String name);
-}
